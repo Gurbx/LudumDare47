2106a9da0a5893e04d4c7289d4cb24380c0c5e7a
Philip <philip.lindberg101@gmail.com>
2020-10-03 21:22:30 +0300

    UI stuff

---
+++ LudumDare47/Assets/Scripts/UI/TextUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUI : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private Text text;
+
+    public void ShowText(string displayText)
+    {
+        text.text = displayText;
+        animator.SetBool("IsVisible", true);
+    }
+
+    public void HideText()
+    {
+        animator.SetBool("IsVisible", false);
+    }
+}
---
+++ LudumDare47/Assets/Scripts/World/TutorialPopup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPopup : MonoBehaviour
+{
+    [TextArea][SerializeField] private string text;
+    [SerializeField] private TextUI textUI;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            textUI.ShowText(text);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            textUI.HideText();
+        }
+    }
+}
