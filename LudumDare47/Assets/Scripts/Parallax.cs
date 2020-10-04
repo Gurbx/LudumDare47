eb13d64dc1d7482b9321d94f1abcd9291a9759f1
Philip <philip.lindberg101@gmail.com>
2020-10-04 13:14:10 +0300

    Added bg stuff

---
+++ LudumDare47/Assets/Scripts/Parallax.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float scrollSpeed;
+
+    private float yOffset;
+    private Vector3 pos = Vector3.zero;
+
+    private void Awake()
+    {
+        yOffset = transform.position.y;
+    }
+
+    void Update()
+    {
+        pos.x = playerTransform.position.x * scrollSpeed;
+        pos.y = transform.position.y * 0.05f + yOffset;
+
+        transform.position = pos;
+    }
+}
