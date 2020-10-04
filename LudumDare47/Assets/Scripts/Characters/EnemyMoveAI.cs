87fdfaa48c41d7ff0c7b49d64eae575f6df40a40
Philip <philip.lindberg101@gmail.com>
2020-10-04 12:44:47 +0300

    Sprites

---
+++ LudumDare47/Assets/Scripts/Characters/EnemyMoveAI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveAI : MonoBehaviour
+{
+    [SerializeField] private CharacterController characterController;
+
+    private void FixedUpdate()
+    {
+        characterController.Move(0, false, false);
+    }
+}
